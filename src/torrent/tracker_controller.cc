734da07c3bd2a8be19000d1f6fd672604fe19605
Rosen Penev <rosenp@gmail.com>
2024-12-19 15:05:20 -0800

    unique_ptr conversions
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/torrent/tracker_controller.cc
+++ src/torrent/tracker_controller.cc
@@ -84,9 +84,9 @@ TrackerController::current_send_state() const {
 }
 
 TrackerController::TrackerController(TrackerList* trackers) :
-  m_flags(0),
-  m_tracker_list(trackers),
-  m_private(new tracker_controller_private) {
+    m_flags(0),
+    m_tracker_list(trackers),
+    m_private(std::make_unique<tracker_controller_private>()) {
 
   m_private->task_timeout.slot() = std::bind(&TrackerController::do_timeout, this);
   m_private->task_scrape.slot() = std::bind(&TrackerController::do_scrape, this);
@@ -95,7 +95,6 @@ TrackerController::TrackerController(TrackerList* trackers) :
 TrackerController::~TrackerController() {
   priority_queue_erase(&taskScheduler, &m_private->task_timeout);
   priority_queue_erase(&taskScheduler, &m_private->task_scrape);
-  delete m_private;
 }
 
 rak::priority_item*
