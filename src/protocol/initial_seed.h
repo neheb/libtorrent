847ff371df194cacdc73170225d79fa344181fe7
Rosen Penev <rosenp@gmail.com>
2025-04-06 15:14:39 -0700

    unique_ptr conversions
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/protocol/initial_seed.h
+++ src/protocol/initial_seed.h
@@ -80,7 +80,7 @@ private:
   uint32_t            m_nextChunk{0};
   uint32_t            m_chunksLeft;
   DownloadMain*       m_download;
-  PeerInfo**          m_peerChunks;
+  std::unique_ptr<PeerInfo*[]> m_peerChunks;
 };
 
 }
