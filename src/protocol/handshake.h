ff19204531e512460f523609fc797b8f1e615144
Rosen Penev <rosenp@gmail.com>
2025-05-10 19:07:31 -0700

    remove some delete calls
    unique_ptr being a member of a class effectively deletes its copy
    operations.
    
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/protocol/handshake.h
+++ src/protocol/handshake.h
@@ -61,8 +61,6 @@ public:
 
   Handshake(SocketFd fd, HandshakeManager* m, int encryption_options);
   ~Handshake() override;
-  Handshake(const Handshake&) = delete;
-  Handshake& operator=(const Handshake&) = delete;
 
   const char*         type_name() const override    { return "handshake"; }
 
