7f772925e0f8e1454c5b7e991b8ae68535f28b3e
Rosen Penev <rosenp@gmail.com>
2025-06-12 21:40:52 -0700

    resolver: reduce captures
    Signed-off-by: Rosen Penev <rosenp@gmail.com>

--- src/torrent/net/resolver.cc
+++ src/torrent/net/resolver.cc
@@ -21,15 +21,13 @@ Resolver::init() {
 
 void
 Resolver::resolve_both(void* requester, const std::string& hostname, int family, both_callback&& callback) {
-  auto cb = [this, requester, hostname, family, callback = std::move(callback)]() mutable {
-    auto fn = [this, requester, callback = std::move(callback)](sin_shared_ptr sin, sin6_shared_ptr sin6, int err) mutable {
-      m_thread->callback(requester, std::bind(std::move(callback), std::move(sin), std::move(sin6), err));
-    };
-
-    ThreadNet::thread_net()->udns()->resolve(requester, hostname, family, std::move(fn));
+  auto fn = [this, requester, callback = std::move(callback)](sin_shared_ptr sin, sin6_shared_ptr sin6, int err) mutable {
+    m_thread->callback(requester, std::bind(std::move(callback), std::move(sin), std::move(sin6), err));
   };
 
-  net_thread::callback(requester, std::move(cb));
+  net_thread::callback(requester, [requester, hostname, family, fn] {
+    ThreadNet::thread_net()->udns()->resolve(requester, hostname, family, std::move(fn));
+  });
 }
 
 void
@@ -37,55 +35,51 @@ Resolver::resolve_preferred(void* requester, const std::string& hostname, int fa
   if (preferred != AF_INET && preferred != AF_INET6)
     throw internal_error("Invalid preferred family.");
 
-  auto cb = [this, requester, hostname, family, preferred, callback = std::move(callback)] () mutable {
-    auto fn = [this, requester, preferred, callback = std::move(callback)](const auto& sin, const auto& sin6, int err) mutable {
-      sa_shared_ptr result(nullptr, sa_free);
+  auto fn = [this, requester, preferred, callback = std::move(callback)](const auto& sin, const auto& sin6, int err) mutable {
+    sa_shared_ptr result(nullptr, sa_free);
 
-      if (err == 0) {
-        if (sin != nullptr && sin6 != nullptr) {
-          if (preferred == AF_INET)
-            result = std::reinterpret_pointer_cast<sockaddr>(sin);
-          else
-            result = std::reinterpret_pointer_cast<sockaddr>(sin6);
-
-        } else if (sin != nullptr) {
+    if (err == 0) {
+      if (sin != nullptr && sin6 != nullptr) {
+        if (preferred == AF_INET)
           result = std::reinterpret_pointer_cast<sockaddr>(sin);
-
-        } else if (sin6 != nullptr) {
+        else
           result = std::reinterpret_pointer_cast<sockaddr>(sin6);
-        }
+
+      } else if (sin != nullptr) {
+        result = std::reinterpret_pointer_cast<sockaddr>(sin);
+
+      } else if (sin6 != nullptr) {
+        result = std::reinterpret_pointer_cast<sockaddr>(sin6);
       }
+    }
 
-      m_thread->callback(requester, std::bind(std::move(callback), std::move(result), err));
-    };
-
-    ThreadNet::thread_net()->udns()->resolve(requester, hostname, family, std::move(fn));
+    m_thread->callback(requester, std::bind(std::move(callback), std::move(result), err));
   };
 
-  net_thread::callback(requester, std::move(cb));
+  net_thread::callback(requester, [requester, hostname, family, fn] {
+    ThreadNet::thread_net()->udns()->resolve(requester, hostname, family, std::move(fn));
+  });
 }
 
 void
 Resolver::resolve_specific(void* requester, const std::string& hostname, int family, single_callback&& callback) {
-  auto cb = [this, requester, hostname, family, callback = std::move(callback)]() mutable {
-    auto fn = [this, requester, family, callback = std::move(callback)](const auto& sin, const auto& sin6, int err) mutable {
-      sa_shared_ptr result(nullptr, sa_free);
+  auto fn = [this, requester, family, callback = std::move(callback)](const auto& sin, const auto& sin6, int err) mutable {
+    sa_shared_ptr result(nullptr, sa_free);
 
-      if (err == 0) {
-        if (family == AF_INET && sin != nullptr)
-          result = std::reinterpret_pointer_cast<sockaddr>(sin);
+    if (err == 0) {
+      if (family == AF_INET && sin != nullptr)
+        result = std::reinterpret_pointer_cast<sockaddr>(sin);
 
-        if (family == AF_INET6 && sin6 != nullptr)
-          result = std::reinterpret_pointer_cast<sockaddr>(sin6);
-      }
+      if (family == AF_INET6 && sin6 != nullptr)
+        result = std::reinterpret_pointer_cast<sockaddr>(sin6);
+    }
 
-      m_thread->callback(requester, std::bind(std::move(callback), std::move(result), err));
-    };
-
-    ThreadNet::thread_net()->udns()->resolve(requester, hostname, family, std::move(fn));
+    m_thread->callback(requester, std::bind(std::move(callback), std::move(result), err));
   };
 
-  net_thread::callback(requester, std::move(cb));
+  net_thread::callback(requester, [requester, hostname, family, fn] {
+    ThreadNet::thread_net()->udns()->resolve(requester, hostname, family, std::move(fn));
+  });
 }
 
 void
